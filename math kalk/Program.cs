7756bb9b3b19285f567644617c608305268f6f0a
jacob jolsö <jacke_j_1998@hotmail.se>
2019-10-25 13:48:13 +0200

    Add project files.

---
+++ math kalk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace math_kalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
